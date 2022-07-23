870dc41e29b0085406093585c9064d625662d016
Hafidev <hafidev@hafidevs-iMac.lan>
2022-07-23 17:11:29 +0100

    Handling Errors

--- main.py
+++ main.py
@@ -32,21 +32,17 @@ def generate_password():
 def save():
     """a function that save data entry to a Json file"""
     
-    with open('data.json') as f:
-        data = json.load(f)
-    
-    data.append(
-        {
-            'Website' : website_entry.get(),
-            'Email'   : email_entry.get(),
-            'Password': password_entry.get(),
-            
-        }
-    )
-    
-    website = website_entry.get()
+    website = website_entry.get().capitalize()
     email = email_entry.get()
     my_password = password_entry.get()
+    
+    new_data = {
+        website:
+            {
+                'email'   : email,
+                'password': my_password,
+            }
+    }
     
     if len(website) == 0 or len(my_password) == 0:
         messagebox.showinfo(title='Oops', message='Please make sure all field are not empty!!!')
@@ -58,12 +54,40 @@ def save():
         )
         
         if is_valid:
-            with open('data.json', 'w') as f:
-                json.dump(data, f, indent=4, separators=(',', ': '), ensure_ascii=False)
+            try:
+                with open('data.json', 'r') as f:
+                    data = json.load(f)
+            
+            except FileNotFoundError:
+                with open('data.json', 'w') as f:
+                    json.dump(new_data, f, indent=4, ensure_ascii=False)
+                    messagebox.showinfo(title='Success', message='Your data has been saved successfully.')
+            else:
+                data.update(new_data)
+                with open('data.json', 'w') as f:
+                    json.dump(data, f, indent=4, separators=(',', ': '), ensure_ascii=False)
+                    messagebox.showinfo(title='Success', message='Your data has been saved successfully.')
+            finally:
                 website_entry.delete(0, END)
                 password_entry.delete(0, END)
-        
-        messagebox.showinfo(title='Success', message='Your data has been saved successfully.')
+
+
+def search_password():
+    """Search function"""
+    
+    website = website_entry.get().capitalize()
+    try:
+        with open('data.json') as f:
+            data = json.load(f)
+    except FileNotFoundError:
+        messagebox.showinfo(title='Error', message='No Data File Found. Please create entries.')
+    else:
+        if website in data:
+            email = data[website]['email']
+            password = data[website]['password']
+            messagebox.showinfo(title=website, message=f'Email: {email}\nPassword: {password}')
+        else:
+            messagebox.showinfo(title='Error', message=f'No details for {website} exists!!!')
 
 
 window = Tk()
@@ -86,22 +110,25 @@ password_label = Label(text='Password:')
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=38)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=32)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
 
-email_entry = Entry(width=38)
+email_entry = Entry(width=48)
 email_entry.grid(row=2, column=1, columnspan=2)
 email_entry.insert(0, 'info@my_email.com')
 
-password_entry = Entry(width=21)
+password_entry = Entry(width=32)
 password_entry.grid(row=3, column=1)
 
 # Buttons
-generate_password_btn = Button(text='Generate Password', command=generate_password)
+search_btn = Button(text='Search', width=12, command=search_password)
+search_btn.grid(row=1, column=2)
+
+generate_password_btn = Button(text='Generate Password', width=12, command=generate_password)
 generate_password_btn.grid(row=3, column=2)
 
-add_btn = Button(text='Add', width=36, command=save)
+add_btn = Button(text='Add', width=46, command=save)
 add_btn.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
